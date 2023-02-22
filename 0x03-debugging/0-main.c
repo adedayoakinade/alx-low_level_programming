4147c8f34b790242d5329d10d00731da62d0bdc4
Adedayo Akinade <akinadeadedayo1@gmail.com>
2023-02-22 04:28:53 -0800

    First commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * main - Tests function that prints if integer in positive or negative
- *
+ * main - test function that prints if integer is positive or negative
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
-
 
 	i = 0;
 	positive_or_negative(i);
